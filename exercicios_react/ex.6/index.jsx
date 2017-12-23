6e8b5e098d79de9d7fc2cd870157a2ecbc281c05
fabiomartinsbr <fabiomartinsbr@gmail.com>
2017-12-23 16:31:45 -0200

    Integrando o React-Redux

---
+++ exercicios_react/ex.6/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {combineReducers, createStore} from 'redux'
+import { Provider } from 'react-redux'
+
+import Field from './field'
+import fieldReducer from './fieldReducer'
+
+const reducers = combineReducers ({
+    field: fieldReducer
+})
+
+
+ReactDOM.render(
+
+    <Provider store={createStore( reducers )} >
+        <Field initialValue='Teste3' />
+    </Provider>
+, document.getElementById("app"));

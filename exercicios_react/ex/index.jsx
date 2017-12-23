f00c189a71f22323495a2f443111bc213e29ef7b
fabiomartinsbr <fabiomartinsbr@gmail.com>
2017-12-23 17:06:04 -0200

    React com Redux

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import {combineReducers, createStore} from 'redux'
 import { Provider } from 'react-redux'
 
-import Field from './field'
-import fieldReducer from './fieldReducer'
+import counterReducer from './counterReducer'
+import Counter from './counter'
 
 const reducers = combineReducers ({
-    field: fieldReducer
+    counter: counterReducer
 })
 
 
 ReactDOM.render(
 
     <Provider store={createStore( reducers )} >
-        <Field initialValue='Teste3' />
+        <Counter   />
     </Provider>
 , document.getElementById("app"));

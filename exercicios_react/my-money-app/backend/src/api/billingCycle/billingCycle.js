dcfd7db44c4f28be972aa682aac96228ecbca8ea
fabiomartinsbr <fabiomartinsbr@gmail.com>
2017-12-26 15:22:30 -0200

    backend funcional

--- exercicios_react/my-money-app/backend/src/api/billingCycle/billingCycle.js
+++ exercicios_react/my-money-app/backend/src/api/billingCycle/billingCycle.js
@@ -1,15 +1,15 @@
-const restful = require('node-restful')
 
+const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema ({
     name:{ type:String, required : true},
-    value:{ type: Number, min:0, required :true}
+    value:{ type: Number, min:0, required :[true, 'Informe o valor do crédito.']}
 }) 
 
 const debtSchema = new mongoose.Schema ({
     name:{ type:String, required : true},
-    value:{ type: Number, min:0, required :true},
+    value:{ type: Number, min:0, required :[true, 'Informe o valor do débito.']},
     status: { type:String , required :false, uppercase:true,
         enum: ['PAGO', 'PEDENTE', 'AGENDADO']}
 }) 
--- exercicios_react/my-money-app/backend/src/config/database.js
+++ exercicios_react/my-money-app/backend/src/config/database.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo '{MIN}'."
+
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é menor que o limite mínimo '{MAX}'."
+
+mongoose.Error.messages.Number.enum = "O '{VALUE}' informado não é válido para o atributo '{PATH}'."

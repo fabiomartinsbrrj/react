ecc36d369cf8055a2f1be5a1bb2691f02ce58b7b
fabiomartinsbr <fabiomartinsbr@gmail.com>
2018-01-16 21:32:06 -0200

    Content title

---
+++ exercicios_react/my-money-app/frontend/src/common/template/contentHear.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default props => (
+
+    <section>
+        <h1>{props.title} <small>{props.small}</small></h1>
+    </section>
+
+)
--- exercicios_react/my-money-app/frontend/src/main/app.jsx
+++ exercicios_react/my-money-app/frontend/src/main/app.jsx
@@ -1,7 +1,19 @@
+import '../common/template/dependencies'
 import React from 'react'
+
+import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
+
+import Routes from './routes'
 
 export default props => (
     <div className='wrapper'>
-        <h1>app</h1>
+        <Header />
+        <SideBar />
+        <div className='content-wrapper'>
+            <Routes></Routes>
+        </div>
+        <Footer></Footer>
     </div>
 )

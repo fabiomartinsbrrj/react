097685c07c66bc6731e6864c7357a22ae1bfc777
fabiomartinsbr <fabiomartinsbr@gmail.com>
2018-01-25 22:26:20 -0200

    dashboard works

--- exercicios_react/my-money-app/frontend/src/index.jsx
+++ exercicios_react/my-money-app/frontend/src/index.jsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom'
 
 import App from './main/app'
 
+import { applyMiddleware, createStore} from 'redux'
+
+import {Provider} from 'react-redux'
+import reducers from './main/reducers'
+
+import promise from 'redux-promise'
+
+const store =  applyMiddleware(promise)(createStore)(reducers)
+
 ReactDOM.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
     , document.getElementById('app'))

e3e17bbc116e60a27e5d443cdc0f3b9865f1223f
Fabio Martins <fabiomartins@Fabios-MacBook-Pro.local>
2018-03-19 21:25:33 -0300

    sumOfCredits and sumOfDebts

--- exercicios_react/my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ exercicios_react/my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -10,14 +10,27 @@ import {init} from './billingCycleAction'
 
 import ItemList from './itemList';
 
+import Summary from './summary'
+
 class BillingCycleForm extends Component {
 
     componentWillMount () {
     }
 
+    calculateSummary() {
+        const sum = (t, v) => t + v
+
+        return {
+            sumOfCredits : this.props.credits.map(c => +c.value || 0).reduce( sum ), //o + é mais simples para converter em numero
+            sumOfDebts : this.props.debts.map(d => +d.value || 0).reduce( sum )
+        }
+    }
+
     render() {
         const { handleSubmit, readOnly , credits, debts } = this.props //decorei com redux-form consigo pegar essa propriedade
-          
+        
+        const {sumOfCredits, sumOfDebts} = this.calculateSummary()
+
         return (
 
             <form role='form' onSubmit={handleSubmit}>
@@ -29,6 +42,9 @@ class BillingCycleForm extends Component {
                     <Field name='year' component={LabelAndInput} type='number'  readOnly={readOnly} 
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
                         
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
+
+
                     <ItemList cols='12 6' readOnly={ readOnly } 
                         list={credits} field='credits' legend="Créditos"  />
 
